4e335eba76b241ac8ce43cf1ce42d6ead545a7c2
Jianjia Zhang <jianjia2@illinois.edu>
2020-05-01 16:13:15 -0500

    May 1: add new function: can open new website show more houses

--- app/src/main/java/com/example/finalproject0/MainActivity.java
+++ app/src/main/java/com/example/finalproject0/MainActivity.java
@@ -2,19 +2,43 @@ package com.example.finalproject0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
-    TextView result;
+//    ProgressDialog pd;
+    public static TextView result;
     EditText area, bathroom, bedroom;
-    Button calculate;
+    Button calculate, moreHouse;
     double price;
     double theta0 = 93.5730487, theta1 = 0.0707709889, theta2 = -4.36588035, theta3 = 20.285743, theta4 = 27.742016;
     int areaVal, bathroomVal, bedroomVal;
@@ -41,7 +65,91 @@ public class MainActivity extends AppCompatActivity {
                 result.setText(priceStr);
             }
         });
+
+        moreHouse = (Button) findViewById(R.id.moreHouse);
+        moreHouse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                result.setText("000");
+//                new JsonTask().execute("https://www.homesearchil.com/results-gallery/?");
+                String website = "https://www.homesearchil.com/results-gallery/?";
+                clicked_button(website);
+            }
+        });
+    }
+    public void clicked_button(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 
+
+//    private class JsonTask extends AsyncTask<String, String, String> {
+//
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//
+//            pd = new ProgressDialog(MainActivity.this);
+//            pd.setMessage("Please wait");
+//            pd.setCancelable(false);
+//            pd.show();
+//        }
+//
+//        protected String doInBackground(String... params) {
+//
+//
+//            HttpURLConnection connection = null;
+//            BufferedReader reader = null;
+//
+//            try {
+//                URL url = new URL(params[0]);
+//                connection = (HttpURLConnection) url.openConnection();
+//                connection.connect();
+//
+//
+//                InputStream stream = connection.getInputStream();
+//
+//                reader = new BufferedReader(new InputStreamReader(stream));
+//
+//                StringBuffer buffer = new StringBuffer();
+//                String line = "";
+//
+//                while ((line = reader.readLine()) != null) {
+//                    buffer.append(line+"\n");
+//                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+//
+//                }
+//
+//                return buffer.toString();
+//
+//
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                if (connection != null) {
+//                    connection.disconnect();
+//                }
+//                try {
+//                    if (reader != null) {
+//                        reader.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result0) {
+//            super.onPostExecute(result0);
+//            if (pd.isShowing()){
+//                pd.dismiss();
+//            }
+//            result.setText(result0);
+//        }
+//    }
 
 }
